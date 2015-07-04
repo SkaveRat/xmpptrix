ebd7e530432d7e081e0cd35703d3071331492ab5
SkaveRat <skaverat@funlynx.de>
2015-07-04 02:53:19 +0200

    first prototype of bridge

--- src/index.js
+++ src/index.js
@@ -2,10 +2,47 @@ var restify  = require('restify')
     , Client = require('node-xmpp-client')
     , ltx    = require('node-xmpp-core').ltx;
 
+//Settings
+var mx_host = 'm.skaverat.net';
+var mx_port = 61448;
+var mx_localpart = 'skaverat';
+var access_token ='aeY9ay8wahqu0pheo1zah2sozohcu4Ciexo0eev1ja';
+var xmpp_jid = 'matrix@skaverat.net';
+var xmpp_password = 'f00b4r';
+
+var mx_user = '@' + mx_localpart + ': ' + mx_host;
+var URL_CREATE_ROOM = '/_matrix/client/api/v1/createRoom?access_token=' + access_token + '&user_id=';
+var URL_SEND_MESSAGE = '/_matrix/client/api/v1/rooms/{room_id}/send/m.room.message?access_token={access_token}&user_id={user_id}';
+var server = restify.createServer();
+var http = restify.createJSONClient({url: 'https://'+ mx_host+':' + mx_port, rejectUnauthorized: false});
+server.use(restify.bodyParser({ }));
+var clients = [];
+
+
 function putTransaction(req, res, next) {
-    console.log("incoming");
-    console.log(req.body);
-    console.log(req.body.events[0].content);
+    var type = req.body.events[0].type;
+    var user_id = req.body.events[0].user_id;
+    if(type != 'm.room.message' || user_id != mx_user) {
+        res.send("[]");
+        next();
+        return;
+    }
+
+
+    var room_id = req.body.events[0].room_id;
+    var message = req.body.events[0].content.body;
+    var receiver = getJidByRoom(room_id);
+    var sender = xmpp_jid;
+    var reply = new ltx.Element('message', {
+        to: receiver,
+        from: sender,
+        type: 'chat'
+    });
+    reply.c('body').t(message);
+
+    var client = clients[sender];
+    client.send(reply);
+
     res.send("[]");
     next();
 }
@@ -30,56 +67,72 @@ function getRoomByJid(jid) {
     return foundroom;
 }
 
+
+function getJidByRoom(room_id) {
+    var foundjid = null;
+    rooms.forEach(function (room) {
+        if(room.room_id == room_id) {
+            foundjid = room.jid
+        }
+    });
+    return foundjid;
+}
+
 function roomExists(jid) {
     return !!getRoomByJid(jid) || false
 }
 
 function createRoom(jid) {
-    http.post(URL_CREATE_ROOM + '@xmpp_' + jid + ':m.skaverat.net',{
+    http.post(URL_CREATE_ROOM + '@xmpp_' + jid + ':' + mx_host,{
         visibility: 'private',
-        invite: [user_id]
-    }, function (err) {
-        if(err) console.log(err);
+        invite: [mx_user]
+    }, function (err, req, res) {
+        if(err) {
+            console.log(err)
+        }else{
+            var response = JSON.parse(res.body);
+            rooms.push({
+                room_id: response.room_id,
+                jid: jid
+            });
+        }
     });
 }
 
 function sendMessage(sender, message) {
-    var user_id = '@xmpp_' + sender + ':m.skaverat.net';
+    var user_id = '@xmpp_' + sender + ':' + mx_host;
     var room_id = getRoomByJid(sender);
     var request_uri = URL_SEND_MESSAGE.replace('{room_id}', room_id).replace('{access_token}', access_token).replace('{user_id}', user_id);
     http.post(request_uri, {
         msgtype: 'm.text',
         body: message
     }, function (err) {
-        if(err)
+        if(err) {
             console.log(err);
+        }
     });
 }
 
 var rooms = [
-    {   room_id: '!TZaeCAqTYsmDQQFPbz:m.skaverat.net',
-        jid: 'skaverat@skaverat.net'
-    }
+    //{   room_id: '!TZaeCAqTYsmDQQFPbz:m.skaverat.net',
+    //    jid: 'skaverat@skaverat.net'
+    //}
 ];
 
-var user_id = '@skaverat:m.skaverat.net';
-var access_token ='aeY9ay8wahqu0pheo1zah2sozohcu4Ciexo0eev1ja';
-var URL_CREATE_ROOM = '/_matrix/client/api/v1/createRoom?access_token=' + access_token + '&user_id=';
-var URL_SEND_MESSAGE = '/_matrix/client/api/v1/rooms/{room_id}/send/m.room.message?access_token={access_token}&user_id={user_id}';
-var server = restify.createServer();
-var http = restify.createJSONClient({url: 'https://m.skaverat.net:61448', rejectUnauthorized: false});
-server.use(restify.bodyParser({ }));
-var clients = [];
 
 server.put('/transactions/:transaction', putTransaction);
 
 function prepareClients() {
-    clients.forEach(function (client) {
+    Object.keys(clients).forEach(function (jid) {
+        var client = clients[jid];
         client.on('stanza', function (stanza) {
             if(stanza.is('message') && stanza.getChild('body')) {
                 var message = stanza.getChild('body').children.join('').trim();
                 var sender = stanza.getAttr('from').split('/')[0];
-                sendMessage(sender, message);
+                if(sender != xmpp_jid) {
+                    sendMessage(sender, message);
+                }
+                return;
             }
 
 
@@ -113,10 +166,10 @@ function prepareClients() {
 
 server.listen(61444, function() {
 
-    clients.push(new Client({
-        jid: 'matrix@skaverat.net',
-        password: 'f00b4r'
-    }));
+    clients[xmpp_jid] = new Client({
+        jid: xmpp_jid,
+        password: xmpp_password
+    });
 
 
     prepareClients();
