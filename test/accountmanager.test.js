dfd322f5aac379138f27cf0727340e25d47da63c
SkaveRat <skaverat@funlynx.de>
2015-08-06 21:14:40 +0200

    adding basic unittest

---
+++ test/accountmanager.test.js
@@ -0,0 +1,18 @@
+var   sinon = require('sinon')
+    , proxyquire = require('proxyquire');
+
+var loggerStub = {};
+
+var Accountmanager = proxyquire('../lib/accountmanager', {'./logger': loggerStub});
+
+module.exports.setUp = function (next) {
+    loggerStub.info = sinon.spy();
+    next();
+};
+
+exports.hasConstructor = function (test) {
+    var t = new Accountmanager();
+    test.ok(loggerStub.info.calledOnce);
+    test.done();
+};
+
